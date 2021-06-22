0ef25a79e94751b1e419565dc23b78198da5676b
Thiago Ferreira <thiagogbferreira@gmail.com>
2021-06-22 07:22:43 -0300

    feat: add save option

--- script/screenshot.sh
+++ script/screenshot.sh
@@ -3,8 +3,11 @@
 # import ~/screenshot/$(date +%Y%m%d_%H%M%S).jpg
 #import -window {root, "$(xdo id)"}  ~/screenshot/$(date +%Y%m%d_%H%M%S).jpg
 #import -window {root, "$(xdotool getwindowfocus -f)"}  ~/screenshot/$(date +%Y%m%d_%H%M%S).jpg
-#import ~/screenshot/$(date +%Y%m%d_%H%M%S).jpg
 
+[[ "$1" == "save" ]] && {
+    import ~/screenshot/$(date +%Y%m%d_%H%M%S).jpg
+    exit 0
+}
 #import png:- | xclip -selection clipboard -t image/png
 #import ~/screenshot/$(date +%Y%m%d_%H%M%S).jpg
 slop=$(slop -f "%g") || exit 1
